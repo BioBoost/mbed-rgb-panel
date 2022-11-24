a25a7f6dc609e46adf07db4b6a064754705c46e0
Nico De Witte <nico.dewitte@outlook.com>
2022-11-24 07:23:58 +0100

    Fix include and switch to uint32_t for buffer

--- src/RgbPanel.h
+++ src/RgbPanel.h
@@ -2,34 +2,33 @@
 
 #include "mbed.h"
 
-
-class RgbPanel
-{
-    private: 
-    static const int BUFFER_SIZE = 1024;
+class RgbPanel {
     
-    public:
+  public:
     RgbPanel(
         PinName r0, PinName g0, PinName b0,
         PinName r1, PinName g1, PinName b1,
         PinName a, PinName b, PinName c, PinName d,
         PinName clk, PinName stb, PinName oe);
+  
+  public: 
+    static const int BUFFER_SIZE = 1024;
+    uint32_t buffer[BUFFER_SIZE];
     
-    int buffer[BUFFER_SIZE];
-    
+  public:
     void clear();
     void draw();
+
+  private:
+    inline void clock();
+    inline void latch();
+    inline void blank();
     
-    private:
+  private:
     BusOut rgb0;
     BusOut rgb1;
     BusOut demux;
     DigitalOut clk;
     DigitalOut stb;
     DigitalOut oe;
-    
-    inline void clock();
-    inline void latch();
-    inline void blank();
-    
 };
