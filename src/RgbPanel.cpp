8b4fca76408dabf05cc531e611259dcdebd3e993
Nico De Witte <nico.dewitte@outlook.com>
2022-11-26 10:44:17 +0100

    Fix

--- src/RgbPanel.cpp
+++ src/RgbPanel.cpp
@@ -17,28 +17,63 @@ void RgbPanel::clear() {
   draw();
 }
 
+// The LAT (latch) signal marks the end of a row of data.
+// OE (output enable) switches the LEDs off when transitioning from one row to the next. I think OE is also per shift register and not all at once
+// The CLK (clock) signal marks the arrival of each bit of data.
+
 void RgbPanel::draw() {
+  clk = 0;
+  stb = 0;
+
+  oe = 1;     // disable output
+
   for (int row = 0; row < 16; row++) {
-    demux = row - 1;
-    //wait_us(1000); // disable row ghosting
+    // Fix ghosting by clearing previous selected row !
+    // (Expensive as it doubles the time for drawing a panel)
+    for (int i = 0; i < 32; i++) {
+      rgb0 = 0;
+      rgb1 = 0;
+      clk = 1;      // Clock in
+      clk = 0;      // Clock disable
+    }
+    stb = 1;        // End of row data
+    stb = 0;        // Disable
+
+    // Switch row
+    oe = 1;         // Disable output
+    demux = row;
+    oe = 0;
+
+
+    // Output actual data
     for (int i = 0; i < 32; i++) {
       rgb0 = buffer[(32*row) + i];
       rgb1 = buffer[(32*row) + i + (BUFFER_SIZE / 2)];
-      clock();
+
+      clk = 1;      // Clock in
+      clk = 0;      // Clock disable
     }
-    latch();
+
+    stb = 1;        // End of row data
+    stb = 0;        // Disable
+
   }
-  blank();
+
+
+
+  oe = 0;   // enable output
+
+
 }
 
 inline void RgbPanel::clock() {
-  clk = 1;
   clk = 0;
+  clk = 1;
 }
 
 inline void RgbPanel::latch() {
-  stb = 1;
   stb = 0;
+  stb = 1;
 }
 
 inline void RgbPanel::blank() {
